d697d1dacd00ac70fe491030f44674e87d505130
Zenonist <phonarnun@hotmail.com>
2025-05-07 01:34:32 +0700

    feat: Add autocomplete to search with category

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -8,15 +8,18 @@ import { CountdownFormat } from "./entity/CountdownFormat";
 
 function App() {
   const [countdowns, setCountdowns] = useState<CountdownFormat[]>([]);
-  const [selectedCountdown, setSelectedCountdown] = useState<string | null>(null);
+  const [selectedTitle, setSelectedTitle] = useState<string | null>(null);
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
 
   useEffect(() => {
     getCountdowns();
-  },[])
+  }, []);
 
   const getCountdowns = () => {
-    const URL = import.meta.env.VITE_BACKEND_URL + "/" + encodeURIComponent("timer");
-    axios.get(URL)
+    const URL =
+      import.meta.env.VITE_BACKEND_URL + "/" + encodeURIComponent("timer");
+    axios
+      .get(URL)
       .then((response) => {
         console.log(response.data);
         setCountdowns(response.data);
@@ -24,43 +27,69 @@ function App() {
       .catch((error) => {
         console.error("Error fetching data:", error);
         setCountdowns(mock_data);
-      })
-  }
+      });
+  };
 
-  const selectedCountdownData = countdowns.find(
-    (countdown) => countdown.title === selectedCountdown
-  );
+  const selectedCountdownData = () => {
+    if (selectedTitle && selectedCategory) {
+      return countdowns.filter(
+        (countdown) =>
+          countdown.title === selectedTitle &&
+          countdown.category.name === selectedCategory
+      );
+    } else if (selectedTitle && !selectedCategory) {
+      return countdowns.filter(
+        (countdown) => countdown.title === selectedTitle
+      );
+    } else if (!selectedTitle && selectedCategory) {
+      return countdowns.filter(
+        (countdown) => countdown.category.name === selectedCategory
+      );
+    } else {
+      return null;
+    }
+  };
 
   return (
     <div>
-      <Navbar createTimer={() => getCountdowns()} timers={countdowns} onSelectTimer={
-        (timer: string | null) => {
-          setSelectedCountdown(timer);
-        }
-      }/>
-      <div className="text-transparent text-4xl bg-gradient-to-b from-blue-500 to-purple-700 bg-clip-text">
+      <Navbar
+        createTimer={() => getCountdowns()}
+        timers={countdowns}
+        onSelectTimer={(timer: string | null) => {
+          setSelectedTitle(timer);
+        }}
+        onSelectCategory={(category: string | null) => {
+          setSelectedCategory(category);
+        }}
+      />
+      <div className="text-transparent text-4xl bg-gradient-to-b from-blue-500 to-purple-700 bg-clip-text p-2">
         Countdown timer website
       </div>
       <div>
-        {!selectedCountdown ? countdowns.map((countdown) => (
-          <Countdown
-            key={countdown.id}
-            _id={countdown.id}
-            _title={countdown.title}
-            _description={countdown.description}
-            _category={countdown.category}
-            _dueDate={countdown.dueDate}
-            onDelete={() => getCountdowns()}
-          />
-        )) : ( selectedCountdownData &&
-          <Countdown
-            _id={selectedCountdownData.id}
-            _title={selectedCountdownData.title}
-            _description={selectedCountdownData.description}
-            _category={selectedCountdownData.category}
-            _dueDate={selectedCountdownData.dueDate}
-          />
-        )}
+        {/* It requires () because it is function */}
+        {!selectedCountdownData()
+          ? countdowns.map((countdown) => (
+              <Countdown
+                key={countdown.id}
+                _id={countdown.id}
+                _title={countdown.title}
+                _description={countdown.description}
+                _category={countdown.category}
+                _dueDate={countdown.dueDate}
+                onDelete={() => getCountdowns()}
+              />
+            ))
+          : selectedCountdownData()?.map((countdown: CountdownFormat) => (
+              <Countdown
+                key={countdown.id}
+                _id={countdown.id}
+                _title={countdown.title}
+                _description={countdown.description}
+                _category={countdown.category}
+                _dueDate={countdown.dueDate}
+                onDelete={() => getCountdowns()}
+              />
+            ))}
       </div>
     </div>
   );
