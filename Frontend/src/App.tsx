9af7b13ba15f81e5984119d2526c660f2f76a39a
Zenonist <phonarnun@hotmail.com>
2025-05-07 14:23:42 +0700

    feat: Add Pagination (Change page) function

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,13 +3,25 @@ import Navbar from "./components/Navbar";
 import Countdown from "./components/Countdown";
 import axios from "axios";
 import { mock_data } from "./mock/mockData";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CountdownFormat } from "./entity/CountdownFormat";
+import { Pagination } from "@mui/material";
 
 function App() {
+  // * Variable
+  const cardsPerPage = 3
+
+  // * Use state
   const [countdowns, setCountdowns] = useState<CountdownFormat[]>([]);
   const [selectedTitle, setSelectedTitle] = useState<string | null>(null);
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+  const [page, setPage] = useState(1);
+  
+  const currentCards = (countdowns: CountdownFormat[]) => {
+    const result = countdowns.slice((page - 1) * cardsPerPage, page * cardsPerPage);
+    console.log(result);
+    return result;
+  }
 
   useEffect(() => {
     getCountdowns();
@@ -50,6 +62,12 @@ function App() {
     }
   };
 
+  // * Variable for setting the number of pages
+  // If there is selected countdown data, then use the selected countdown data for the number of pages
+  // Otherwise (when is null in this case), use the total countdown data for the number of pages
+  const dataSourceForPageCount = selectedCountdownData() ?? countdowns;
+  const totalPages = Math.ceil(dataSourceForPageCount.length / cardsPerPage);
+
   return (
     <div>
       <Navbar
@@ -66,9 +84,11 @@ function App() {
         Countdown timer website
       </div>
       <div>
+        {/* Cards */}
+
         {/* It requires () because it is function */}
         {!selectedCountdownData()
-          ? countdowns
+          ? currentCards(countdowns)
               // Show unarchived countdowns first
               // NOTE: Sort works by comparing the boolean values if it is same then return 0 (No change) if a is true and b is false then return 1 (a comes first) if a is false and b is true then return -1 (b comes first)
               .sort((a, b) => (a.isArchived === b.isArchived ? 0 : a.isArchived ? 1 : -1))
@@ -85,7 +105,7 @@ function App() {
                   onArchive={() => getCountdowns()}
                 />
               ))
-          : selectedCountdownData()
+          : currentCards(selectedCountdownData() ?? [])
               ?.sort((a, b) => (a.isArchived === b.isArchived ? 0 : a.isArchived ? 1 : -1))
               .map((countdown: CountdownFormat) => (
                 <Countdown
@@ -100,6 +120,30 @@ function App() {
                   onArchive={() => getCountdowns()}
                 />
               ))}
+      </div>
+      <div>
+        {/* Pagination */}
+        <Pagination 
+          count={totalPages}
+          onChange={(_event: React.ChangeEvent<unknown>, page: number) => {
+            setPage(page);
+          }}
+          color="primary"
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "20px",
+            color: "white",
+            // Need to change the color of the pagination item instead of pagination (without item in text)
+            "& .MuiPaginationItem-root": {
+              color: "white",
+            },
+          }}
+          size="large"
+          showFirstButton
+          showLastButton
+        />
+
       </div>
     </div>
   );
